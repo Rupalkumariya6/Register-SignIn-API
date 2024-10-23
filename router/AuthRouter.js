ce8806be9cb90f0914e25d4461741465af8bc705
Rupal Kumariya <166526924+Rupalkumariya6@users.noreply.github.com>
2024-10-23 10:10:02 +0530

    Update AuthRouter.js

--- router/AuthRouter.js
+++ router/AuthRouter.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const user = require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const SECRET_KEY = "hehehehehhe"
 
 router.post('/register', async (req, res) => {
 
